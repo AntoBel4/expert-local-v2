5c8652f0035639c61185013ff923003ab6e29af3
Antoine Estarellas <antoinestarellas@gmail.com>
2026-04-06 18:50:20 +0200

    feat: Ajouter la configuration de contenu pour le blog

---
+++ src/content.config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders'; // <-- Nouveau loader
+
+const blog = defineCollection({
+  // On dit à Astro d'aller chercher les .md dans le dossier blog
+  loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string(),
+    image: z.string().optional(),
+    author: z.string().default('Antoine Estarellas'),
+  })
+});
+
+export const collections = { blog };
